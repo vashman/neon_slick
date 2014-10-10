64bfef4900f62b975c260806747ce2c277f5f2fe
Sunny S <3030712@gmail.com>
2014-10-10 16:02:39 -0700

    Inserted gpl license header into source files.

--- neon_slick/header.php
+++ neon_slick/header.php
@@ -1,3 +1,22 @@
+<?php
+/* Copyright (C) 2014 Sundeep S. Sangha
+
+    This file is a part of neon_slick.
+
+    Neon_slick is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
 <html><head>
 <title><?php \wp_title(); \bloginfo('name'); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
--- neon_slick/page.php
+++ neon_slick/page.php
@@ -1,2 +1,21 @@
-<?php \get_header(); ?>
+<?php
+/* Copyright (C) 2014 Sundeep S. Sangha
+
+    This file is a part of neon_slick.
+
+    Neon_slick is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+      \get_header();
+?>
 <?php \get_footer(); ?>
