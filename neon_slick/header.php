5ef39de8f08e0db40ac75410b0ab4f17d060d85f
Sunny S <3030712@gmail.com>
2014-10-10 03:19:18 -0700

    updated header menu to use custom css
    menu ul a and div all use the nav class now.

--- neon_slick/header.php
+++ neon_slick/header.php
@@ -20,9 +20,19 @@
 <?php
       \wp_nav_menu(array (
         'theme_location' => 'header-menu'
-      , 'container' => 'false'
-      , 'menu_class' => 'nav'
+      , 'menu' => ''
+      , 'container' => false
+      , 'container_class' => ""
+      , 'container_id' => ''
+      , 'menu_class' => ''
+      , 'menu_id' => ''
+      , 'echo' => true
+      , 'fallback_cb' => 'wp_page_menu'
+      , 'before' => ''
+      , 'after' => ''
       , 'items_wrap' => '<ul class="nav"><li class="nav">%3$s</li></ul>'
+      , 'depth' => 0
+      , 'walker' => ''
       ));
 ?>
 </div>
