e6ac3a1c1af87887f1ed3ce7ed1a40b1193ed296
Sunny S <3030712@gmail.com>
2014-10-10 02:10:59 -0700

    updated menu with custom class.

--- neon_slick/header.php
+++ neon_slick/header.php
@@ -1,15 +1,29 @@
-<html>
-<head>
+<html><head>
 <title><?php \wp_title(); \bloginfo('name'); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
 <style>
 <?php include('style.css'); ?>
 </style>
-<?php \wp_head(); ?>
+<?php
+      \wp_head();
+?>
 </head>
 <body>
 <div class="header">
 <div class="logo">?</div>
-<div class="serach"><?php \get_search_form(); ?></div>
-<div class="nav"><?php \wp_nav_menu(array ('theme_location' => 'header-menu')); ?></div>
+<div class="serach">
+<?php
+      \get_search_form();
+?>
+</div>
+<div class="nav">
+<?php
+      \wp_nav_menu(array (
+        'theme_location' => 'header-menu'
+      , 'container' => 'false'
+      , 'menu_class' => 'nav'
+      , 'items_wrap' => '<ul class="nav"><li class="nav">%3$s</li></ul>'
+      ));
+?>
+</div>
 </div>
