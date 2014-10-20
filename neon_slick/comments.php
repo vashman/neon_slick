8d5e0b45ee8a39f88136935ace1238cbaca313b4
Sunny S <3030712@gmail.com>
2014-10-20 04:09:55 -0700

    updated comments.php

--- neon_slick/comments.php
+++ neon_slick/comments.php
@@ -16,6 +16,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+/* Structure
+*/
 
 \comments_number('No Comments', '1 Comment', '% Comments');
 ?>
